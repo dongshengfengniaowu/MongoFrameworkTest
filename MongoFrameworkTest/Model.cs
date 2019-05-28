6957563a455812762795dc9d5ac2ffd5695fe726
irac <ding.dongsheng@fengniaowu.com>
2019-05-28 10:34:30 +0800

    cannot sort with keys that are parallel arrays

--- MongoFrameworkTest/Model.cs
+++ MongoFrameworkTest/Model.cs
@@ -50,149 +50,142 @@ namespace MongoFrameworkTest
         public DeviceMongoContext(IMongoDbConnection Connection) : base(Connection)
         {
             EntityMapping.GetOrCreateDefinition(typeof(DeviceView)).CollectionName = "KC.Fengniaowu.Hebe.Devices";
+            EntityMapping.GetOrCreateDefinition(typeof(DeviceReadingModel)).CollectionName = "KC.Fengniaowu.Hebe.DeviceReadings";
         }
-
+        public MongoDbSet<DeviceReadingModel> DeviceReadingModels { get; set; }
         public MongoDbSet<DeviceView> DeviceModels { get; set; }
     }
-    public class DeviceView : KolibreActorState
+    public class DeviceReadingModel
     {
         /// <summary>
-        ///     设备唯一标识 (UUID)
+        ///     Id
         /// </summary>
+        [Key]
         [Required]
+        public string DeviceReadingId { get; set; }
+
         public string DeviceId { get; set; }
 
-        /// <summary>
-        ///     设备mac地址
-        /// </summary>
         public string DeviceMac { get; set; }
 
-        /// <summary>
-        ///     设备类型
-        /// </summary>
         [Required]
         public string DeviceType { get; set; }
 
-        /// <summary>
-        ///     设备序列号
-        /// </summary>
         public string DeviceSn { get; set; }
 
-        /// <summary>
-        ///     自定义设备名称
-        /// </summary>
         public string DeviceName { get; set; }
 
-        /// <summary>
-        ///     自定义设备描述
-        /// </summary>
+        public string Description { get; set; }
+        [Required]
+        public double DeviceReading { get; set; }
+
+        public double SettlementReading { get; set; }
+
+        public string ServiceID { get; set; }
+
+        public bool Success { get; set; }
+
+        [Required]
+        public DateTimeOffset ReadingTime { get; set; }
+
+        [Required]
+        public string Supplier { get; set; }
+
+        [Required]
+        public bool Enabled { get; set; }
+
+        [Required]
+        public DateTimeOffset CreateTime { get; set; }
+
+        [Required]
+        public DateTimeOffset UpdateTime { get; set; }
+
+        [Column("Data")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public string DataPayload
+        {
+            get => Data.ToJson(JsonSettings.DataJsonSettings);
+            set => Data = new Dictionary<string, string>(value.FromJson<Dictionary<string, string>>(JsonSettings.DataJsonSettings), StringComparer.OrdinalIgnoreCase);
+        }
+
+        [JsonIgnore]
+        [NotMapped]
+        public Dictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
+    }
+    public class DeviceView : KolibreActorState
+    {
+        [Required]
+        public string DeviceId { get; set; }
+        public string DeviceMac { get; set; }
+        [Required]
+        public string DeviceType { get; set; }
+
+        public string DeviceSn { get; set; }
+        public string DeviceName { get; set; }
+
+
         public string Description { get; set; }
 
-        /// <summary>
-        ///     设备所在房源Id (商户定义)
-        /// </summary>
+
         public string HomeId { get; set; }
 
-        /// <summary>
-        ///     设备所在房间Id (商户定义)
-        /// </summary>
+
         public string RoomId { get; set; }
 
-        /// <summary>
-        ///     设备所属商户Id
-        /// </summary>
+
         public string AssetTenancyId { get; set; }
 
-        /// <summary>
-        ///     设备是否关联至蜂鸟平台
-        /// </summary>
+
         public bool IsAssociated { get; set; }
 
-        /// <summary>
-        ///     设备绑定的公寓Id
-        /// </summary>
+
         public string ApartmentId { get; set; }
 
-        /// <summary>
-        ///     设备关联房间Id
-        /// </summary>
+
         public string AssociatedRoomId { get; set; }
 
         public bool IsPublic { get; set; }
 
-        /// <summary>
-        ///     进行关联操作用户的访问凭证
-        /// </summary>
         public string Token { get; set; }
 
-        /// <summary>
-        ///     关联时间
-        /// </summary>
+
         public DateTimeOffset? AssociateTime { get; set; }
 
-        /// <summary>
-        ///     关联人
-        /// </summary>
+
         public string AssociatorName { get; set; }
 
-        /// <summary>
-        ///     绑定网关uuid (门锁)  TODO
-        /// </summary>
+
         public string CenterUUID { get; set; }
 
-        /// <summary>
-        ///     设备绑定时间 (接口返回 - Lock)
-        /// </summary>
         public DateTimeOffset? BindTime { get; set; }
 
-        /// <summary>
-        ///     设备在线状态
-        /// </summary>
         public bool OnOffLine { get; set; }
 
-        /// <summary>
-        ///     最近一次在线状态更新时间
-        /// </summary>
+
         public DateTimeOffset? OnOffTime { get; set; }
 
-        /// <summary>
-        ///     设备电量
-        /// </summary>
+
         public int DevicePower { get; set; }
 
-        /// <summary>
-        ///     最近一次电量刷新时间
-        /// </summary>
+ 
         public DateTimeOffset? PowerRefreshTime { get; set; }
 
-        /// <summary>
-        ///     设备型号
-        /// </summary>
+
         public string Model { get; set; }
 
-        /// <summary>
-        ///     设备型号名称
-        /// </summary>
+
         public string ModelName { get; set; }
 
-        /// <summary>
-        ///     设备信号值 (门锁)
-        /// </summary>
+
         public int Lqi { get; set; }
 
-        /// <summary>
-        ///     信号刷新时间
-        /// </summary>
+
         public DateTimeOffset? LqiRefreshTime { get; set; }
 
-        /// <summary>
-        ///     设备版本信息 (Json字符串)
-        /// </summary>
+
         public string Versions { get; set; }
 
-        /// <summary>
-        ///     设备供应商
-        /// </summary>
+
         public string Supplier { get; set; }
 
         public string OpenId { get; set; }
@@ -207,9 +200,6 @@ namespace MongoFrameworkTest
             set => OtherData = new Dictionary<string, string>(value.FromJson<Dictionary<string, string>>(JsonSettings.DataJsonSettings), StringComparer.OrdinalIgnoreCase);
         }
 
-        /// <summary>
-        ///     其他数据
-        /// </summary>
         [JsonIgnore]
         [NotMapped]
         public Dictionary<string, string> OtherData { get; set; } = new Dictionary<string, string>();
